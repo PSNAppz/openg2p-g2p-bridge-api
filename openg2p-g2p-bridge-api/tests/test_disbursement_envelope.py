eed0710b35c26635dfa0256cdcf83461d6c7a34b
PSNAppZ <thepsnarayanan@gmail.com>
2024-09-02 15:33:31 +0530

    Disbursement Envelope Cancellation UT

--- openg2p-g2p-bridge-api/tests/test_disbursement_envelope.py
+++ openg2p-g2p-bridge-api/tests/test_disbursement_envelope.py
@@ -33,7 +33,7 @@ def mock_create_disbursement_envelope(is_valid, error_code=None):
         total_disbursement_amount=5000.00,
         disbursement_schedule_date=datetime.date(datetime.utcnow()),
     )
-    disbursement_response = DisbursementEnvelopeResponse(
+    disbursement_envelope_response = DisbursementEnvelopeResponse(
         header=SyncResponseHeader(
             message_id="",
             message_ts=datetime.now().isoformat(),
@@ -43,7 +43,7 @@ def mock_create_disbursement_envelope(is_valid, error_code=None):
         ),
         message=disbursement_envelope_payload,
     )
-    return disbursement_response
+    return disbursement_envelope_response
 
 
 @pytest.mark.asyncio
@@ -154,89 +154,124 @@ async def test_create_disbursement_envelope_errors(
     ), f"The response did not match the expected error response for {error_code}."
 
 
-#
-# def mock_cancel_disbursement_envelope(is_valid, error_code=None):
-#     if not is_valid:
-#         raise DisbursementEnvelopeException(
-#             code=error_code, message=f"{error_code} error."
-#         )
-#
-#     return DisbursementEnvelopePayload(
-#         disbursement_envelope_id="env123",
-#         benefit_program_mnemonic="TEST123",
-#         disbursement_frequency="Monthly",
-#         cycle_code_mnemonic="CYCLE42",
-#         number_of_beneficiaries=100,
-#         number_of_disbursements=100,
-#         total_disbursement_amount=5000.00,
-#         disbursement_schedule_date=datetime.date(datetime.utcnow()),
-#     )
-#
-#
-# @pytest.mark.asyncio
-# @patch("openg2p_g2p_bridge_api.services.DisbursementEnvelopeService.get_component")
-# async def test_cancel_disbursement_envelope_success(mock_service_get_component):
-#     mock_service_instance = AsyncMock()
-#     mock_service_instance.cancel_disbursement_envelope = AsyncMock(
-#         return_value=mock_cancel_disbursement_envelope(True)
-#     )
-#     mock_service_instance.construct_disbursement_envelope_success_response = AsyncMock()
-#
-#     mock_service_get_component.return_value = mock_service_instance
-#
-#     successful_payload = mock_cancel_disbursement_envelope(True)
-#     expected_response = DisbursementEnvelopeResponse(
-#         response_status=ResponseStatus.SUCCESS, response_payload=successful_payload
-#     )
-#     mock_service_instance.construct_disbursement_envelope_success_response.return_value = (
-#         expected_response
-#     )
-#
-#     controller = DisbursementEnvelopeController()
-#     request_payload = DisbursementEnvelopeRequest(
-#         request_payload=DisbursementEnvelopePayload(disbursement_envelope_id="env123")
-#     )
-#
-#     actual_response = await controller.cancel_disbursement_envelope(request_payload)
-#     assert actual_response == expected_response
-#
-#
-# @pytest.mark.asyncio
-# @patch("openg2p_g2p_bridge_api.services.DisbursementEnvelopeService.get_component")
-# @pytest.mark.parametrize(
-#     "error_code",
-#     [
-#         G2PBridgeErrorCodes.DISBURSEMENT_ENVELOPE_NOT_FOUND,
-#         G2PBridgeErrorCodes.DISBURSEMENT_ENVELOPE_ALREADY_CANCELED,
-#     ],
-# )
-# async def test_cancel_disbursement_envelope_failure(
-#     mock_service_get_component, error_code
-# ):
-#     mock_service_instance = AsyncMock()
-#     mock_service_instance.cancel_disbursement_envelope.side_effect = (
-#         lambda request: mock_cancel_disbursement_envelope(False, error_code)
-#     )
-#     mock_service_instance.construct_disbursement_envelope_error_response = AsyncMock()
-#
-#     mock_service_get_component.return_value = mock_service_instance
-#
-#     error_response = DisbursementEnvelopeResponse(
-#         response_status=ResponseStatus.FAILURE,
-#         response_error_code=error_code.value,
-#     )
-#     mock_service_instance.construct_disbursement_envelope_error_response.return_value = (
-#         error_response
-#     )
-#
-#     controller = DisbursementEnvelopeController()
-#     request_payload = DisbursementEnvelopeRequest(
-#         request_payload=DisbursementEnvelopePayload(
-#             disbursement_envelope_id="env123"  # Assuming this ID triggers the error
-#         )
-#     )
-#
-#     actual_response = await controller.cancel_disbursement_envelope(request_payload)
-#     assert (
-#         actual_response == error_response
-#     ), f"The response for {error_code} did not match the expected error response."
+def mock_cancel_disbursement_envelope(is_valid, error_code=None):
+    if not is_valid:
+        raise DisbursementEnvelopeException(
+            code=error_code, message=f"{error_code} error."
+        )
+
+    disbursement_envelope_payload = DisbursementEnvelopePayload(
+        disbursement_envelope_id="env123",
+        benefit_program_mnemonic="TEST123",
+        disbursement_frequency="Monthly",
+        cycle_code_mnemonic="CYCLE42",
+        number_of_beneficiaries=100,
+        number_of_disbursements=100,
+        total_disbursement_amount=5000.00,
+        disbursement_schedule_date=datetime.date(datetime.utcnow()),
+    )
+    disbursement_envelope_response = DisbursementEnvelopeResponse(
+        header=SyncResponseHeader(
+            message_id="",
+            message_ts=datetime.now().isoformat(),
+            action="",
+            status=StatusEnum.succ,
+            status_reason_message="",
+        ),
+        message=disbursement_envelope_payload,
+    )
+    return disbursement_envelope_response
+
+
+@pytest.mark.asyncio
+@patch("openg2p_g2p_bridge_api.services.DisbursementEnvelopeService.get_component")
+async def test_cancel_disbursement_envelope_success(mock_service_get_component):
+    mock_service_instance = AsyncMock()
+    mock_service_instance.cancel_disbursement_envelope = AsyncMock(
+        return_value=mock_cancel_disbursement_envelope(True)
+    )
+    mock_service_instance.construct_disbursement_envelope_success_response = AsyncMock()
+
+    mock_service_get_component.return_value = mock_service_instance
+
+    expected_response = mock_cancel_disbursement_envelope(True)
+
+    mock_service_instance.construct_disbursement_envelope_success_response.return_value = (
+        expected_response
+    )
+
+    controller = DisbursementEnvelopeController()
+    request_payload = DisbursementEnvelopeRequest(
+        header=RequestHeader(
+            message_id="123",
+            message_ts=datetime.now().isoformat(),
+            action="",
+            sender_id="",
+            sender_uri="",
+            receiver_id="",
+            total_count=1,
+            is_msg_encrypted=False,
+        ),
+        message=DisbursementEnvelopePayload(disbursement_envelope_id="env123")
+    )
+
+    actual_response = await controller.cancel_disbursement_envelope(request_payload)
+    assert actual_response == expected_response
+
+
+
+@pytest.mark.asyncio
+@patch("openg2p_g2p_bridge_api.services.DisbursementEnvelopeService.get_component")
+@pytest.mark.parametrize(
+    "error_code",
+    [
+        G2PBridgeErrorCodes.DISBURSEMENT_ENVELOPE_NOT_FOUND,
+        G2PBridgeErrorCodes.DISBURSEMENT_ENVELOPE_ALREADY_CANCELED,
+    ],
+)
+async def test_cancel_disbursement_envelope_failure(
+    mock_service_get_component, error_code
+):
+    mock_service_instance = AsyncMock()
+    mock_service_instance.cancel_disbursement_envelope.side_effect = (
+        lambda request: mock_cancel_disbursement_envelope(False, error_code)
+    )
+    mock_service_instance.construct_disbursement_envelope_error_response = AsyncMock()
+
+    mock_service_get_component.return_value = mock_service_instance
+
+    error_response = DisbursementEnvelopeResponse(
+        header = SyncResponseHeader(
+            message_id="",
+            message_ts=datetime.now().isoformat(),
+            action="",
+            status=StatusEnum.rjct,
+            status_reason_message=error_code,
+        ),
+    )
+    mock_service_instance.construct_disbursement_envelope_error_response.return_value = (
+        error_response
+    )
+
+    controller = DisbursementEnvelopeController()
+    request_payload = DisbursementEnvelopePayload(
+        disbursement_envelope_id="env123"  # Assuming this ID triggers the error
+    )
+    request_payload = DisbursementEnvelopeRequest(
+        header=RequestHeader(
+            message_id="123",
+            message_ts=datetime.now().isoformat(),
+            action="",
+            sender_id="",
+            sender_uri="",
+            receiver_id="",
+            total_count=1,
+            is_msg_encrypted=False,
+        ),
+        message=request_payload,
+    )
+
+    actual_response = await controller.cancel_disbursement_envelope(request_payload)
+    assert (
+        actual_response == error_response
+    ), f"The response for {error_code} did not match the expected error response."
