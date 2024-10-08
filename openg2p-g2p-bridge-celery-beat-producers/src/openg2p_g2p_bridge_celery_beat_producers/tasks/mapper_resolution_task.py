ec2778aecb0b4e79c02117d51cb0b021f8617196
PSNAppZ <thepsnarayanan@gmail.com>
2024-10-08 15:12:27 +0530

    Pre commit fixes

--- openg2p-g2p-bridge-celery-beat-producers/src/openg2p_g2p_bridge_celery_beat_producers/tasks/mapper_resolution_task.py
+++ openg2p-g2p-bridge-celery-beat-producers/src/openg2p_g2p_bridge_celery_beat_producers/tasks/mapper_resolution_task.py
@@ -36,7 +36,8 @@ def mapper_resolution_beat_producer():
         )
         for mapper_resolution_batch_status in mapper_resolution_batch_statuses:
             _logger.info(
-                f"{mapper_resolution_batch_status.resolution_attempts} / {_config.mapper_resolve_attempts} attempts done")
+                f"{mapper_resolution_batch_status.resolution_attempts} / {_config.mapper_resolve_attempts} attempts done"
+            )
 
             _logger.info(
                 f"Sending mapper_resolution_worker task for mapper_resolution_batch_id: {mapper_resolution_batch_status.mapper_resolution_batch_id}"
